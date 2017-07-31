34f3069199d496815ab11c9e0731f4072b49d352
elliotj <elliotj@live.unc.edu>
2017-07-31 01:37:30 -0400

    Trying to make this work

--- write-to-database.sh
+++ write-to-database.sh
@@ -2,12 +2,12 @@
 # Set Variables
 # Set MySQL credentials
 MYSQLUSER=root
-MYSQLPASS=root
+MYSQLPASS=changethis
 # Set database and table names
 MYDATABASE=Survey
-MYTABLE=Survey-Questions
+MYTABLE=Questions
 # Place data in the MySQL secure database
-sudo cp ./tmp.csv/var/lib/mysql-files/
+sudo cp ./tmp.csv /var/lib/mysql-files/
 echo "Data copied to secure directory"
 # Check if database exists, and if not, then create it
 echo "Checking for database"
@@ -25,11 +25,11 @@ if [ "$DBCHECK" == "$MYTABLE" ]; then
 echo "Table Exists"
 else
 echo "Table does not exist. Creating table now."
-mysql -u"$MYSQLUSER" -p"$MYTABLE" -e"CREATE TABLE $MYTABLE Hometown VARCHAR(255), Genre VARCHAR(255), Weather VARCHAR(255), Animals VARCHAR(255), Flower VARCHAR(255), ALTER TABLE $MYTABLE ADD PRIMARY KEY (Hometown)" $MYDATABASE
+mysql -u"$MYSQLUSER" -p"$MYSQLPASS" -e "CREATE TABLE $MYTABLE (Hometown VARCHAR(255), Genre VARCHAR(255), Weather VARCHAR(255), Animals VARCHAR(255), Flower VARCHAR(255)), (ALTER TABLE $MYTABLE ADD PRIMARY KEY (Hometown));" $MYDATABASE
 fi
 # Write data from tmp.csv to database table
-echo "Writing information from $MYTABLE to $MYDATABASE"
-mysql -u"$MYSQLUSER" -p"$MYSQLPASS" -e "LOAD DATA INFILE '/var/lib/mysql-files/tmp.csv' INTO TABLE $MYTABLE FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"';" $MYDATABASE
+echo "Writing information to $MYTABLE in $MYDATABASE"
+mysql -u"$MYSQLUSER" -p"$MYSQLPASS" -e "LOAD DATA INFILE '/var/lib/mysql-files/tmp.csv' INTO TABLE $MYTABLE FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';" $MYDATABASE
 echo "Data written successfully"
 # Dump current version of database into export file
 echo "Survey data dumped to `date --iso-8601=seconds`-$MYDATABASE.sql"
